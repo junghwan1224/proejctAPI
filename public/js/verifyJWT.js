5c5a8d1cc90f1a8d67f48272c915325d866884cd
spark <junghwan1224@gmail.com>
2019-11-01 15:10:46 +0900

    feat: modify handle err logic

--- public/js/verifyJWT.js
+++ public/js/verifyJWT.js
@@ -3,16 +3,22 @@ const DEV_SECRET = process.env.DEV_SECRET;
 
 function verifyJWT(req, res) {
     const token = req.headers.authorization;
-    jwt.verify(token, DEV_SECRET, (err, decoded) => {
+
+    const verify = jwt.verify(token, DEV_SECRET, (err, decoded) => {
         if(err) {
-            return res.status(403).send({
-                status: "jwt verify failed",
-                message: "유효한 유저의 정보가 아닙니다. 다시 로그인해주세요."
-            });
+            console.log("jwt verify error");
+            console.log(err);
+            // return res.status(403).send({
+            //     status: "jwt verify failed",
+            //     message: "유효한 유저의 정보가 아닙니다. 다시 로그인해주세요."
+            // });
+            return null;
         }
 
         return decoded.id;
     });
+
+    return verify;
 }
 
 module.exports = verifyJWT;
