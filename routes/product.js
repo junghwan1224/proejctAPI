42635b287223854a017faa30203ad24fcfa063c4
Chianti Scarlett <chianti.scarlett@gmail.com>
2019-11-07 15:32:34 +0900

    feat: add maker_number

--- routes/product.js
+++ routes/product.js
@@ -202,6 +202,7 @@ router.get("/ark/product-list", function(req, res, next) {
           oe_number: product.oe_number,
           price: product.price,
           maker: product.product_abstract.maker,
+          maker_number: product.product_abstract.maker_number,
           brand: product.brand,
           model: product.model,
           quantity: product.product_abstract.stock
