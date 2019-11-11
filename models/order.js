9d58fbfadad1aefab83777139389f1dae83c33e2
spark <junghwan1224@gmail.com>
2019-11-11 17:17:20 +0900

    feat: modify relation order with delivery

--- models/order.js
+++ models/order.js
@@ -47,7 +47,7 @@ module.exports = (sequelize, DataTypes) => {
       onUpdate: "cascade"
     });
 
-    order.hasOne(models.delivery);
+    // order.hasOne(models.delivery);
   };
   return order;
 };
