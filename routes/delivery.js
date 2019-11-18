5be88aa9ed0db7c9c917d13e09f2dac486dbec3e
spark <junghwan1224@gmail.com>
2019-11-18 15:22:48 +0900

    feat: add get delivery list api for ark

--- routes/delivery.js
+++ routes/delivery.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const asyncHandler = require('express-async-handler');
 const verifyToken = require("./verifyToken");
 
+const Account = require("../models").account;
 const Delivery = require("../models").delivery;
 const Order = require("../models").order;
 const Product = require("../models").product;
@@ -86,6 +87,94 @@ router.put("/update/location/:order_id", asyncHandler(async (req, res) => {
     );
 
     res.status(200).send({ message: "update success" });
+}));
+
+
+/************ Ark ************/
+
+router.get("/ark/list", asyncHandler(async (req, res) => {
+    try {
+        const transaction = await models.sequelize.transaction();
+
+        const delivery = await Delivery.findAll({
+            attributes: ["order_id", "status", "location", "arrived_at"],
+            transaction
+        });
+        const deliveryInfo = delivery.map(d => d.dataValues);
+        const orderInfo = deliveryInfo.map(d => {
+            const { order_id } = d;
+            return Order.findAll({
+                where: { imp_uid: order_id },
+                attributes: ["imp_uid", "amount", "quantity", "pay_method", "memo", "updatedAt"],
+                include: [{
+                    model: Product,
+                    required: true,
+                    include: [{
+                        model: ProductAbstract,
+                        required: true,
+                        attributes: ["image", "maker", "maker_number", "type"]
+                    }]
+                },
+                {
+                    model: Account,
+                    required: true,
+                    attributes: ["phone", "name", "crn", "mileage", "email"],
+                }],
+                transaction
+            });
+        });
+        await Promise.all(orderInfo);
+
+        await transaction.commit();
+
+        res.status(200).send({ orderInfo, deliveryInfo });
+    }
+
+    catch(err) {
+        console.log(err);
+        res.status(403).send({ message: "에러가 발생했습니다. 다시 시도해주세요." });
+    }
+}));
+
+router.get("/ark/user", asyncHandler(async (req, res) => {
+    try {
+        const { account_id } = req.query;
+        const transaction = await models.sequelize.transaction();
+
+        const delivery = await Delivery.findAll({
+            where: { account_id },
+            attributes: ["order_id", "status", "location", "arrived_at"],
+            transaction
+        });
+        const deliveryInfo = delivery.map(d => d.dataValues);
+        const orderInfo = deliveryInfo.map(d => {
+            const { order_id } = d;
+            return Order.findAll({
+                where: { imp_uid: order_id },
+                attributes: ["imp_uid", "amount", "quantity", "pay_method", "memo", "updatedAt"],
+                include: [{
+                    model: Product,
+                    required: true,
+                    include: [{
+                        model: ProductAbstract,
+                        required: true,
+                        attributes: ["image", "maker", "maker_number", "type"]
+                    }]
+                }],
+                transaction
+            });
+        });
+        await Promise.all(orderInfo);
+
+        await transaction.commit();
+
+        res.status(200).send({ orderInfo, deliveryInfo });
+    }
+
+    catch(err) {
+        console.log(err);
+        res.status(403).send({ message: "에러가 발생했습니다. 다시 시도해주세요." });
+    }
 }));
 
 module.exports = router;
