dea1a09ce4c4eb3a906cfbf97c35c4633a9ca343
spark <junghwan1224@gmail.com>
2019-12-04 11:19:32 +0900

    feat: add cancel credit api for ark

--- routes/transaction.js
+++ routes/transaction.js
@@ -7,6 +7,7 @@ const { Op } = sequelize;
 const models = require("../models");
 const Account = require("../models").account;
 const Order = require("../models").order;
+const Delivery = require("../models").delivery;
 const Product = require("../models").product;
 const ProductAbstract = require("../models").product_abstract;
 
@@ -92,12 +93,12 @@ router.post("/ark/create", asyncHandler(async (req, res) => {
 }));
 
 // update transaction status
-router.post("/ark/credit-complete", asyncHandler(async (req, res) => {
+router.post("/ark/complete-credit", asyncHandler(async (req, res) => {
     try {
         const { merchant_uid } = req.body;
 
         await Order.update({
-            status: "paid"
+            status: "credit"
         }, {
             where: {
                 merchant_uid
@@ -115,13 +116,107 @@ router.post("/ark/credit-complete", asyncHandler(async (req, res) => {
 // cancel deal with credit
 router.post("/ark/cancel-credit", asyncHandler(async (req, res) => {
     try {
-        // update status value paid or not paid to canceled
-        // update stock
+        const { merchant_uid } = req.body;
+        const transaction = await models.sequelize.transaction();
+
+        // stock update
+        const orders = await Order.findAll({
+            where: { merchant_uid },
+            transaction
+        });
+        const productsId = orders.map(o => o.dataValues.product_id);
+        const productsQuantity = orders.map(o => o.dataValues.quantity);
+
+        const prodArr = productsId.map((p, idx) => ({
+            "id": p,
+            "quantity": productsQuantity[idx]
+        }));
+        prodArr.sort((a, b) => { if(a.id < b.id) return -1; else return 1; });
+
+        const abstractsIds = await Product.findAll({
+            where: { id: { [Op.in]: prodArr.map(e => e.id) } },
+            include: [ProductAbstract],
+            transaction
+        });
+
+        const abstArr = abstractsIds.map(
+            product => {
+                const { dataValues } = product;
+                const { product_abstract } = dataValues;
+                const prop = product_abstract.dataValues;
+    
+                return prop.id;
+            }
+        );
+
+        const abstObj = abstArr.map((abst, idx) => ({
+            "id": abst,
+            "quantity": prodArr[idx].quantity
+        }));
+    
+        const abstractMap = abstObj.reduce((prev, cur) => {
+            let count = prev.get(cur.id) || 0;
+            prev.set(cur.id, cur.quantity + count);
+            return prev;
+        }, new Map());
+    
+        const mapToArr = [...abstractMap].map( ([id, quantity]) => { return {id, quantity} });
+        mapToArr.sort((a, b) => { if(a.id < b.id) return -1; else return 1; });
+    
+        const abstracts = await ProductAbstract.findAll({
+            where: {
+                id: { [Op.in]: abstObj.map(e => e.id) }
+            },
+            transaction
+        });
+    
+        const updatedProducts = abstracts.map(
+            product => {
+                return product.dataValues;
+            }
+        );
+    
+        updatedProducts.forEach(
+         (product, idx) => {
+             product.stock += mapToArr[idx].quantity;
+         }
+        );
+    
+        await ProductAbstract.bulkCreate(updatedProducts, {
+            updateOnDuplicate: ["stock"],
+            transaction
+        });
+
+        await Order.update({
+            status: "cancelled"
+        }, {
+            where: { merchant_uid },
+            transaction
+        });
+
+        await transaction.commit();
+
+        res.status(201).send({ message: "결제 취소가 완료되었습니다." });
     }
     catch(err) {
         console.log(err);
         res.status(403).send({ message: "에러가 발생했습니다. 다시 시도해주세요." });
     }
+}));
+
+router.post("/ark/create-delivery", asyncHandler(async (req, res) => {
+    const { account_id, merchant_uid } = req.body;
+
+    await Delivery.create({
+        account_id,
+        delivery_num: merchant_uid.slice(7),
+        order_id: merchant_uid,
+        status: "",
+        location: "",
+        arrived_at: Date.now()
+    });
+
+    res.status(201).send({ message: "success" });
 }));
 
 module.exports = router;
