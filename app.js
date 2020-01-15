47fa09ee3ff1e3cb7add240e9cecd3141cece08f
spark <junghwan1224@gmail.com>
2020-01-15 17:27:56 +0900

    fix: change db password null to DB_PASSWORD in env

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var options = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
-  password: null,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB
 };
 
