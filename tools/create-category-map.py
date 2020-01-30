659e42f395a16ba728c8865f6fbb013a736bdfff
Chianti Scarlett <chianti.scarlett@gmail.com>
2020-01-30 17:08:49 +0900

    상용차 카테고리도 추가되게끔 코드 업데이트

--- tools/create-category-map.py
+++ tools/create-category-map.py
@@ -1,9 +1,9 @@
 import requests
 import json
-from pprint import pprint
+from pprint import pformat
 
-# BASE_ROUTE = 'http://localhost:3001'
-BASE_ROUTE = "http://13.124.230.55:3001"
+BASE_ROUTE = 'http://localhost:3001'
+# BASE_ROUTE = "http://13.124.230.55:3001"
 PRODUCT_ROUTE = '/api/product/fetch-all'
 
 
@@ -14,26 +14,31 @@ def main():
         return
 
     map_file = {}
-
     products = json.loads(response.text)
+
     for product in products:
+        if product['category'] not in map_file.keys():
+            map_file[product['category']] = {}
+
         if not product['end_year']:
             product['end_year'] = 2019
         years = range(product['start_year'], product['end_year']+1)
         for year in years:
             # Create year if not exist:
-            if year not in map_file.keys():
-                map_file[year] = {}
+            if year not in map_file[product['category']].keys():
+                map_file[product['category']][year] = {}
 
             # Create brand if not exist:
-            if product['brand'] not in map_file[year].keys():
-                map_file[year][product['brand']] = []
+            if product['brand'] not in map_file[product['category']][year].keys():
+                map_file[product['category']][year][product['brand']] = []
 
             # Add model if not exist:
-            if product['model'] not in map_file[year][product['brand']]:
-                map_file[year][product['brand']].append(product['model'])
+            if product['model'] not in map_file[product['category']][year][product['brand']]:
+                map_file[product['category']][year][product['brand']].append(
+                    product['model'])
 
-    pprint(map_file)
+    with open("output.json", 'w') as fp:
+        fp.write(str(map_file))
 
 
 if __name__ == "__main__":
