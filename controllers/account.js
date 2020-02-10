676a2d060990d47383332b29c18bc82f05b17f14
Chianti Scarlett <chianti.scarlett@gmail.com>
2020-02-10 13:45:09 +0900

    주석 추가 및 에러 메세지 변경

--- controllers/account.js
+++ controllers/account.js
@@ -4,14 +4,30 @@ const Account = require("../models").account;
 const AccountLevel = require("../models").account_level;
 
 exports.createByUser = async (req, res) => {
+  /* If phone, password, name, type are not included, return 400: */
   if (
     !(req.body.phone && req.body.password && req.body.name && req.body.type)
   ) {
     return res.status(400).send({
-      message: "No sufficient data."
+      message: "필요한 정보를 모두 입력해주세요."
     });
   }
 
+  /* If the phone number is already registered, raise 400: */
+  try {
+    const response = await Account.findOne({
+      where: { phone: req.body.phone }
+    });
+    if (response) {
+      return res.status(400).send({ message: "이미 등록된 전화번호입니다." });
+    }
+  } catch (err) {
+    return res
+      .status(400)
+      .send({ message: "에러가 발생했습니다. 잠시 후 다시 시도해주세요." });
+  }
+
+  /* Create account, with level set to "NORMAL" : */
   try {
     await Account.create({
       phone: req.body.phone,
@@ -22,7 +38,6 @@ exports.createByUser = async (req, res) => {
       email: req.body.email,
       level: "NORMAL"
     });
-
     return res.status(201).send();
   } catch (error) {
     return res
@@ -32,6 +47,7 @@ exports.createByUser = async (req, res) => {
 };
 
 exports.readByUser = async (req, res) => {
+  /* Allow pre-defined attributes only:  */
   const ALLOWED_ATTRIBUTES = [
     "id",
     "phone",
@@ -45,6 +61,7 @@ exports.readByUser = async (req, res) => {
   const fields = req.query.fields || "";
   const account_id = req.query.account_id;
 
+  /* Fetch account data including level attributes:  */
   const response = await Account.findOne({
     where: { id: account_id },
     attributes: ALLOWED_ATTRIBUTES,
@@ -58,6 +75,7 @@ exports.readByUser = async (req, res) => {
     ]
   });
 
+  /* Return attributes based on the user request(parameters): */
   try {
     if (response) {
       const attributes = fields.toLowerCase().split(",");
@@ -81,13 +99,32 @@ exports.readByUser = async (req, res) => {
 };
 
 exports.updateByUser = async (req, res) => {
+  /* Allow pre-defined attributes only:  */
   const POSSIBLE_ATTRIBUTES = ["email", "crn", "name", "type", "password"];
-
   let newData = {};
   POSSIBLE_ATTRIBUTES.map(
     attribute => (newData[attribute] = req.body[attribute])
   );
 
+  /* Verify whether the user exists: */
+  try {
+    const response = await Account.findOne({
+      where: {
+        iid: req.query.account_id
+      }
+    });
+    if (!response) {
+      return res
+        .status(400)
+        .send({ message: "해당 ID에 대한 유저 데이터가 존재하지 않습니다." });
+    }
+  } catch (err) {
+    return res
+      .status(400)
+      .send({ message: "에러가 발생했습니다. 다시 시도해주세요." });
+  }
+
+  /* Return attributes based on the user request(parameters): */
   try {
     await Account.update(newData, {
       where: {
@@ -104,13 +141,14 @@ exports.updateByUser = async (req, res) => {
 };
 
 exports.deleteByAdmin = async (req, res) => {
+  /* Send 400 if account_id is not given: */
   try {
     if (!req.body.account_id) {
       return res.status(400).send({
-        message: "No sufficient data."
+        message: "필요한 정보를 모두 입력해주세요."
       });
     }
-
+    /* Verify whether the user exists: */
     const response = await Account.findOne({
       where: { id: req.body.account_id }
     });
@@ -118,6 +156,7 @@ exports.deleteByAdmin = async (req, res) => {
       return res.status(400).send({ message: "User not found." });
     }
 
+    /* Delete account data from the DB: */
     await Account.destroy({
       where: { id: req.body.account_id },
       limit: 1
