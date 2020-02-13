f8cdfcfd18cc176d8da3921c15b38207606407e8
spark <junghwan1224@gmail.com>
2020-02-13 11:38:48 +0900

    토큰 인증 미들웨어 추가

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,41 @@
 module.exports = app => {
+  const verifyToken = require("./verifyToken");
+
   const account = require("../controllers/account");
   const accountLevel = require("../controllers/accountLevel");
   const ADMIN_ROUTE = "/admin";
   app
     .route(ADMIN_ROUTE + "/account")
-    .all((req, res, next) => {
-      next();
-    })
+    .all(verifyToken.authAdmin)
     .delete(account.deleteByAdmin);
 
   app
     .route(ADMIN_ROUTE + "/account-level")
-    .all((req, res, next) => {
-      next();
-    })
+    .all(verifyToken.authAdmin)
     .post(accountLevel.createByAdmin)
     .get(accountLevel.readByAdmin);
+
+  app
+    .route(`${ADMIN_ROUTE}/article`)
+    .all(verifyToken.authAdmin)
+    .get()
+    .post()
+    .put()
+    .delete();
+
+  app
+    .route(`${ADMIN_ROUTE}/credit-transaction`)
+    .all(verifyToken.authAdmin)
+    .get()
+    .post()
+    .put()
+    .delete();
+  
+  app
+    .route(`${ADMIN_ROUTE}/credit-transaction/list`)
+    .all(verifyToken.authAdmin)
+    .get()
+    .post()
+    .put()
+    .delete();
 };
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,94 @@
 module.exports = app => {
+  const verifyToken = require("./verifyToken");
+
   const account = require("../controllers/account");
 
   app
     .route("/account")
-    .all((req, res, next) => {
-      next();
-    })
+    .all(verifyToken.authUser)
     .get(account.readByUser)
-    .post(account.createByUser)
     .put(account.updateByUser);
+  
+  app
+    .post(account.createByUser)
+
+  app
+    .route("/basket")
+    .all(verifyToken.authUser)
+    .get()
+    .post()
+    .put()
+    .delete();
+
+  app
+    .route("/delivery")
+    .all(verifyToken.authUser)
+    .get()
+    .post()
+    .put()
+    .delete();
+
+  app
+    .route("/delivery/list")
+    .all(verifyToken.authUser)
+    .get()
+    .post()
+    .put()
+    .delete();
+
+  app
+    .route("/favorite")
+    .all(verifyToken.authUser)
+    .get()
+    .post()
+    .put()
+    .delete();
+
+  app
+    .route("/inquiry")
+    .all(verifyToken.authUser)
+    .get()
+    .post()
+    .put()
+    .delete();
+
+  app
+    .route("/product")
+    .all(verifyToken.authUser)
+    .get()
+    .post()
+    .put()
+    .delete();
+
+  app
+    .route("/payment")
+    .all(verifyToken.authUser)
+    .get()
+    .post()
+    .put()
+    .delete();
+
+  app
+    .route("/order")
+    .all(verifyToken.authUser)
+    .get()
+    .post()
+    .put()
+    .delete();
+  
+  app
+    .route("/receipt")
+    .all(verifyToken.authUser)
+    .get()
+    .post()
+    .put()
+    .delete();
+  
+  app
+    .route("/receipt-external")
+    .all(verifyToken.authUser)
+    .get()
+    .post()
+    .put()
+    .delete();
 };
