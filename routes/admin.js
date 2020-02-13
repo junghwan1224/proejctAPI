d6087a7892765bc955ed65cb03910d820c9773e0
Chianti Scarlett <chianti.scarlett@gmail.com>
2020-02-13 15:45:00 +0900

    Admin  테이블 생성 및 CRUD 추가

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,8 @@
 module.exports = app => {
   const account = require("../controllers/account");
   const accountLevel = require("../controllers/accountLevel");
+  const admin = require("../controllers/admin");
+
   const ADMIN_ROUTE = "/admin";
   app
     .route(ADMIN_ROUTE + "/account")
@@ -8,6 +10,16 @@ module.exports = app => {
       next();
     })
     .delete(account.deleteByAdmin);
+
+  app
+    .route(ADMIN_ROUTE + "/admin")
+    .all((req, res, next) => {
+      next();
+    })
+    .post(admin.createByAdmin)
+    .get(admin.readByAdmin)
+    .put(admin.updateByAdmin)
+    .delete(admin.deleteByAdmin);
 
   app
     .route(ADMIN_ROUTE + "/account-level")
