b2668dbabb55898b9f6aa257a1f739074b0d401a
spark <junghwan1224@gmail.com>
2020-02-13 11:52:25 +0900

    토큰 인증 함수 유저용/관리자용 분리

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,6 +1,6 @@
-var express = require("express");
-var router = express.Router();
 const jwt = require("jsonwebtoken");
+const Account = require("../models/account");
+const Admin = require();
 
 const DEV_SECRET = process.env.DEV_SECRET;
 
@@ -8,27 +8,105 @@ const DEV_SECRET = process.env.DEV_SECRET;
  * Validate view access by verifying given token.
  */
 
-const verifyToken = (req, res, next) => {
-  const token = req.headers.authorization;
+// const verifyToken = (req, res, next) => {
+//   const token = req.headers.authorization;
 
-  const verify = jwt.verify(token, DEV_SECRET, (err, decoded) => {
-    if (err) {
-      console.log("jwt verify error");
-      return null;
-    }
+//   const verify = jwt.verify(token, DEV_SECRET, (err, decoded) => {
+//     if (err) {
+//       console.log("jwt verify error");
+//       return null;
+//     }
 
+//     return decoded.id;
+//   });
+
+//   if (verify) {
+//     req.account_id = verify;
+//     next();
+//   } else {
+//     return res.status(403).send({
+//       status: "jwt verify failed",
+//       message: "유효한 유저의 정보가 아닙니다. 다시 로그인해주세요."
+//     });
+//   }
+// };
+
+// Verify Token function user
+const verifyUser = async token => {
+  const accountId = jwt.verify(token, DEV_SECRET, (err, decoded) => {
+    if(err) { return null; }
     return decoded.id;
   });
 
-  if (verify) {
-    req.account_id = verify;
-    next();
-  } else {
-    return res.status(403).send({
-      status: "jwt verify failed",
-      message: "유효한 유저의 정보가 아닙니다. 다시 로그인해주세요."
+  if(accountId) {
+    // find in account
+    const account = await Account.findOne({
+      where: { id: accountId }
     });
+
+    if(account) {
+      return accountId;
+    }
+    else {
+      return null;
+    }
+  }
+  else {
+    return null;
   }
 };
 
-module.exports = verifyToken;
+// Verify Token function admin
+const verifyAdmin = async token => {
+  const adminId = jwt.verify(token, DEV_SECRET, (err, decoded) => {
+    if(err) { return null; }
+    return decoded.id;
+  });
+
+  if(adminId) {
+    // find in admin
+    const admin = await Admin.findOne({
+      where: { id: adminId }
+    });
+
+    if(admin) {
+      return adminId;
+    }
+    else {
+      return null;
+    }
+  }
+  else {
+    return null;
+  }
+};
+
+// authenticate user
+const authUser = (req, res, next) => {
+  const { authorization } = req.headers;
+  const id = verifyUser(authorization);
+
+  if(id) {
+    req.account_id = id;
+    next();
+  }
+  else {
+    return res.status(403).send();
+  }
+};
+
+// authenticate admin
+const authAdmin = (req, res, next) => {
+  const { authorization } = req.headers;
+  const id = verifyAdmin(authorization);
+
+  if(id) {
+    req.admin_id = id;
+    next();
+  }
+  else {
+    return res.status(403).send();
+  }
+};
+
+module.exports = { authUser, authAdmin };
