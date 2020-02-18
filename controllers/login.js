c5e49306b434ddb3ff142faf7d705e76343f42a3
spark <junghwan1224@gmail.com>
2020-02-18 11:05:24 +0900

    에러 상태 및 에러메세지 키값명 변경

--- controllers/login.js
+++ controllers/login.js
@@ -13,11 +13,11 @@ exports.loginByUser = async (req, res) => {
         const { phone, password } = req.body;
 
         if (phone === undefined || phone === null || phone.length === 0) {
-            return res.status(403).send({ message: "연락처를 입력해주세요." });
+            return res.status(400).send({ message: "연락처를 입력해주세요." });
         }
 
         if (password === undefined || password === null || password.length === 0) {
-            return res.status(403).send({ message: "비밀번호를 입력해주세요." });
+            return res.status(400).send({ message: "비밀번호를 입력해주세요." });
         }
 
         const account = await Account.findOne({
@@ -49,7 +49,7 @@ exports.loginByUser = async (req, res) => {
     catch(err) {
         res
             .status(400)
-            .send({ error_message: "아이디 또는 비밀번호가 일치하지 않습니다." });
+            .send({ message: "아이디 또는 비밀번호가 일치하지 않습니다." });
     }
 };
 
@@ -58,11 +58,11 @@ exports.loginByAdmin = async (req, res) => {
         const { email, password } = req.body;
 
         if (email === undefined || email === null || email.length === 0) {
-            return res.status(403).send({ message: "연락처를 입력해주세요." });
+            return res.status(400).send({ message: "연락처를 입력해주세요." });
         }
 
         if (password === undefined || password === null || password.length === 0) {
-            return res.status(403).send({ message: "비밀번호를 입력해주세요." });
+            return res.status(400).send({ message: "비밀번호를 입력해주세요." });
         }
 
         const admin = await Admin.findOne({
@@ -93,6 +93,6 @@ exports.loginByAdmin = async (req, res) => {
     catch (err) {
         res
             .status(400)
-            .send({ error_message: "아이디 또는 비밀번호가 일치하지 않습니다." });
+            .send({ message: "아이디 또는 비밀번호가 일치하지 않습니다." });
     }
 };
