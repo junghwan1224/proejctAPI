19f019d5b976e3c2e969282001b4b997291fd5d6
Chianti Scarlett <chianti.scarlett@gmail.com>
2020-02-20 12:28:46 +0900

    로그인 로직 수정

--- controllers/login.js
+++ controllers/login.js
@@ -9,17 +9,16 @@ const Account = require("../models").account;
 const DEV_SECRET = process.env.DEV_SECRET;
 
 exports.loginByUser = async (req, res) => {
+  const { phone, password } = req.body;
+
+  if (phone === undefined || phone === null || phone.length === 0) {
+    return res.status(400).send({ message: "연락처를 입력해주세요." });
+  }
+  if (password === undefined || password === null || password.length === 0) {
+    return res.status(400).send({ message: "비밀번호를 입력해주세요." });
+  }
+
   try {
-    const { phone, password } = req.body;
-
-    if (phone === undefined || phone === null || phone.length === 0) {
-      return res.status(400).send({ message: "연락처를 입력해주세요." });
-    }
-
-    if (password === undefined || password === null || password.length === 0) {
-      return res.status(400).send({ message: "비밀번호를 입력해주세요." });
-    }
-
     const account = await Account.findOne({
       where: {
         phone
@@ -35,35 +34,40 @@ exports.loginByUser = async (req, res) => {
         });
 
         res.cookie("user", token);
-        res.status(200).send({
+        return res.status(200).send({
           id,
           name: name,
           phone,
           type,
           token
         });
+      } else {
+        return res
+          .status(400)
+          .send({ message: "아이디 또는 비밀번호가 일치하지 않습니다." });
       }
+    } else {
+      return res.status(400).send({ message: "가입되지 않은 전화번호입니다." });
     }
   } catch (err) {
-    console.log(err);
-    res
-      .status(400)
-      .send({ message: "아이디 또는 비밀번호가 일치하지 않습니다." });
+    return res.status(400).send({
+      message: "에러가 발생했습니다. 잠시 후 다시 시도해주세요."
+    });
   }
 };
 
 exports.loginByAdmin = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (email === undefined || email === null || email.length === 0) {
+    return res.status(400).send({ message: "연락처를 입력해주세요." });
+  }
+
+  if (password === undefined || password === null || password.length === 0) {
+    return res.status(400).send({ message: "비밀번호를 입력해주세요." });
+  }
+
   try {
-    const { email, password } = req.body;
-
-    if (email === undefined || email === null || email.length === 0) {
-      return res.status(400).send({ message: "연락처를 입력해주세요." });
-    }
-
-    if (password === undefined || password === null || password.length === 0) {
-      return res.status(400).send({ message: "비밀번호를 입력해주세요." });
-    }
-
     const admin = await Admin.findOne({
       where: {
         email
@@ -86,11 +90,19 @@ exports.loginByAdmin = async (req, res) => {
           code,
           token
         });
+      } else {
+        return res
+          .status(400)
+          .send({ message: "아이디 또는 비밀번호가 일치하지 않습니다." });
       }
+    } else {
+      res.status(400).send({
+        message: "가입되지 않은 이메일 주소입니다."
+      });
     }
   } catch (err) {
     res
       .status(400)
-      .send({ message: "아이디 또는 비밀번호가 일치하지 않습니다." });
+      .send({ message: "에러가 발생했습니다. 잠시 후 다시 시도해주세요." });
   }
 };
