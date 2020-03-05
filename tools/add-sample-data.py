7c15d7931f5acbc460daec857b7e6b38c9a5571e
Chianti Scarlett <chianti.scarlett@gmail.com>
2020-03-05 17:42:35 +0900

    랜덤 샘플 description_images 추가

--- tools/add-sample-data.py
+++ tools/add-sample-data.py
@@ -27,7 +27,9 @@ def main():
                                          raw_product['type']),
                                      attributes=get_sample_attributes(
                                          raw_product['type'], raw_product['classification']),
-                                     tags=raw_product['tags'])
+                                     tags=raw_product['tags'],
+                                     description_images=get_sample_description_images()
+                                     )
         tackle(response, inspect.getframeinfo(inspect.currentframe()).lineno)
 
     print('----------------------------------------------------------')
@@ -41,6 +43,39 @@ def get_sample_attributes(attribute_type, classification):
            '브레이크패드': ['소모품', '하체부품']}
     classification_map = {'CAR': '승용차', 'COM': '상용차'}
     return ",".join(MAP[attribute_type]+[classification_map[classification]])
+
+
+def get_sample_description_images():
+    sample_images = ['https://thumbnail12.coupangcdn.com/thumbnails/remote/q89/image/retail/images/2018/03/12/18/3/dd10ca05-1dfd-4e67-b6c9-0867d5ce5430.jpg',
+                     'https://thumbnail11.coupangcdn.com/thumbnails/remote/q89/image/retail/images/2018/07/23/16/4/de0bac55-53bb-4355-86df-18c84980853b.jpg',
+                     'https://thumbnail11.coupangcdn.com/thumbnails/remote/q89/image/retail/images/2018/12/28/16/7/ec51de1f-3fa1-44ce-b106-8b830bf88732.jpg',
+                     'https://thumbnail13.coupangcdn.com/thumbnails/remote/q89/image/vendor_inventory/a066/84d8b261776a35cd70671aeb89a1d93ff19b141b2c65d26eebf80d8fe344.jpg',
+                     'https://thumbnail15.coupangcdn.com/thumbnails/remote/q89/image/product/content/vendorItem/2019/05/30/448313834/ec88a4e6-a982-4c6f-bab9-25fafb7359af.jpg',
+                     'https://thumbnail15.coupangcdn.com/thumbnails/remote/q89/image/retail/images/2019/05/14/17/0/9ab3741e-badf-40af-9713-1b2d1a7a2470.jpg',
+                     'https://thumbnail14.coupangcdn.com/thumbnails/remote/q89/image/retail/images/2019/08/21/10/6/90031130-f162-4dfd-ac6e-89c4d72c0d10.jpg',
+                     'https://thumbnail11.coupangcdn.com/thumbnails/remote/q89/image/product/content/vendorItem/2019/08/01/520542436/6d3b6ed6-0c82-4e55-825d-2cf9245e9035.jpg',
+                     'https://thumbnail12.coupangcdn.com/thumbnails/remote/q89/image/retail/images/2019/10/25/17/4/a8e9269b-571b-42e0-810a-92c9b188a8eb.jpg',
+                     'https://thumbnail15.coupangcdn.com/thumbnails/remote/q89/image/product/content/vendorItem/2017/11/06/160392179/d55ab7fe-b627-444a-95e7-baad7bb81168.jpg',
+                     'https://thumbnail13.coupangcdn.com/thumbnails/remote/q89/image/product/content/vendorItem/2019/06/03/435615188/2a42d190-a812-47aa-b3ec-d28f1d325c48.jpg',
+                     'https://thumbnail14.coupangcdn.com/thumbnails/remote/q89/image/product/content/vendorItem/2019/04/12/276324145/0409246b-3107-4211-ad73-aad40c9cd88c.jpg',
+                     'https://thumbnail15.coupangcdn.com/thumbnails/remote/q89/image/product/content/vendorItem/2019/05/10/86048945/5887aa6a-728b-4a90-bbba-67c545382ddc.jpg',
+                     'https://thumbnail13.coupangcdn.com/thumbnails/remote/q89/image/retail/images/256219007592664-db075c37-19a5-4b8b-be22-209a263c19cb.jpg',
+                     'https://thumbnail13.coupangcdn.com/thumbnails/remote/q89/image/retail/images/2018/10/12/19/3/a085d8f6-6d00-4ded-86f3-6d1ba32df9ae.jpg',
+                     'https://thumbnail13.coupangcdn.com/thumbnails/remote/q89/image/product/content/vendorItem/2019/05/17/522121914/692966ec-9f2a-4b27-b654-85bb6a4848a7.jpg',
+                     'https://thumbnail12.coupangcdn.com/thumbnails/remote/q89/image/retail/images/493200132634812-139878df-b5b4-4b4a-9fc9-a0ae8b6a0a54.jpg',
+                     'https://thumbnail11.coupangcdn.com/thumbnails/remote/q89/image/retail/images/2017/06/07/18/5/10e11868-0c8a-4e2a-a235-4b98f502ed09.jpg',
+                     'http://image1.coupangcdn.com/image/vendor_inventory/608d/42ace008c88a847f7e6abb082cded2a1fd3734f8f53d3b324cd707a1bc88.png',
+                     'https://thumbnail11.coupangcdn.com/thumbnails/remote/q89/image/product/content/vendorItem/2019/08/16/709349330/06f83086-7fd2-4b66-887b-8ece88399e78.jpg', 'https://thumbnail11.coupangcdn.com/thumbnails/remote/q89/image/product/content/vendorItem/2017/04/04/6640485/22ebaf94-890e-4a31-81d6-156ab79b87d6.jpg',
+                     'http://image1.coupangcdn.com/image/vendor_inventory/e1cd/2af2b7062e033a986d91d623d0c683b21ea78cadedd6f06486e9fb5750bc.jpg',
+                     'http://img1a.coupangcdn.com/image/vendor_inventory/images/2018/03/08/11/0/1d3414d1-f4db-47f8-8355-a5425c93c14e.jpeg',
+                     'https://thumbnail13.coupangcdn.com/thumbnails/remote/q89/image/retail/images/2018/10/23/17/9/4f392399-d6ea-44c4-95f4-47e5fd460842.jpg',
+                     'https://thumbnail12.coupangcdn.com/thumbnails/remote/q89/image/retail/images/2019/03/22/15/7/719076a9-1cc8-4f5b-8b07-7def357ef0f2.jpg',
+                     'https://thumbnail15.coupangcdn.com/thumbnails/remote/q89/image/retail/images/31639121395414-290d90fb-872f-439b-a7dd-2ee266937622.jpg',
+                     'https://thumbnail11.coupangcdn.com/thumbnails/remote/q89/image/retail/images/2017/09/18/10/6/51704e80-4212-4c58-aba1-03d8774dde73.jpg',
+                     'https://thumbnail14.coupangcdn.com/thumbnails/remote/q89/image/retail/images/2018/11/22/14/5/58dc2d78-31b5-4afe-9e82-3c58e80a3bbe.jpg',
+                     'https://thumbnail14.coupangcdn.com/thumbnails/remote/q89/image/retail/images/78515406730703-a2665a9f-e5dd-4a6d-a083-d99669c9fb96.jpg'
+                     ]
+    return sample_images[randrange(0, len(sample_images))]
 
 
 def get_sample_images(image_type):
