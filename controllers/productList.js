59510f106d5d8a967facdcf0ec3442389c30bfc1
Chianti Scarlett <chianti.scarlett@gmail.com>
2020-05-11 12:54:19 +0900

    Raw Query문을 통해 판매량 가져오는 코드 추가

--- controllers/productList.js
+++ controllers/productList.js
@@ -3,8 +3,23 @@
 const Product = require("../models").product;
 const Account = require("../models").account;
 const AccountLevel = require("../models").account_level;
-const Sequelize = require("sequelize");
 const { verifyToken } = require("../routes/verifyToken");
+const Sequelize = require("sequelize");
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.json")[env];
+
+/** Define sequelize in order to use ::raw_query method:: */
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
 
 const { Op } = Sequelize;
 const calculateDiscount = require("./common/discount").calculateDiscount;
@@ -121,67 +136,28 @@ exports.readByUser = async (req, res) => {
 };
 
 exports.readByAdmin = async (req, res) => {
-  const method = req.query.method || "";
-  const methodMap = {
-    QUERY: ["query"],
-    CAR: ["brand", "model"],
-    TYPE: ["type"],
-    ID: ["concatenatedID"],
-    "*": [],
-  };
-
-  /** Raise 400 for invalid method key: */
-  if (!Object.keys(methodMap).includes(method.toUpperCase()))
-    return res
-      .status(400)
-      .send({ message: "필요한 정보를 모두 입력해주세요." });
-
-  /** Check out if request has enough arguments: */
-  let queryArguments = Object.keys(req.query);
-  for (const key of methodMap[method.toUpperCase()]) {
-    if (!queryArguments.includes(key))
-      return res
-        .status(400)
-        .send({ message: "필요한 정보를 모두 입력해주세요." });
-  }
-
-  /** Set searchField: */
-  let searchField = { is_public: true };
-  let orderField = [["models", "ASC"]];
-  if (method.toUpperCase() === "QUERY") {
-    searchField[Op.or] = [
-      { oe_number: { [Op.like]: `%${req.query.query}%` } },
-      { maker_number: { [Op.like]: `%${req.query.query}%` } },
-    ];
-  } else if (method.toUpperCase() === "TYPE") {
-    searchField.type = { [Op.like]: `%${req.query.type}%` };
-  } else if (method.toUpperCase() === "CAR") {
-    searchField.models = {
-      [Op.like]: `%${[
-        req.query.brand.toUpperCase(),
-        req.query.model.toUpperCase(),
-      ].join("$$")}%`,
-    };
-  } else if (method.toUpperCase() === "ID") {
-    searchField.where = Sequelize.where(
-      Sequelize.fn("LOCATE", Sequelize.col("id"), req.query.concatenatedID),
-      Sequelize.Op.ne,
-      0
-    );
-    orderField = [["id", "ASC"]];
-  } else if (method === "*") {
-  } else {
-    return res.status(400).send({ message: "유효하지 않은 접근입니다." });
-  }
-
   /* Fetch products and apply discount_rate: */
   try {
-    const response = await Product.findAll({
-      where: searchField,
-      order: orderField,
-      attributes: { exclude: ["createdAt", "updatedAt"] },
-    });
-
+    const response = await sequelize.query(
+      `with numbered_table as (
+          with grouped_table as (
+            select products.*, 
+                   sum(orders.quantity) as sales_quantity
+            from products, orders
+            where products.id=orders.product_id and orders.status="paid" 
+            group by orders.product_id
+            union
+            select *, 0 as sales_quantity
+            from products
+          )
+          select *,
+                 row_number() over (partition by id order by id) as rn
+          from grouped_table
+        )
+        select * from numbered_table where rn=1;
+   `,
+      { type: sequelize.QueryTypes.SELECT }
+    );
     return res.status(200).send(response);
   } catch (err) {
     console.log(err);
