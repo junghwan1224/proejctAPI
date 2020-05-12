ff9ac5888d6ea0db529d2f3574cf78139fe79fda
Chianti Scarlett <chianti.scarlett@gmail.com>
2020-05-12 17:44:09 +0900

    스태프 모델 + DB 추가

---
+++ migrations/0002-create-staff.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const uuid = require("uuid");
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("staffs", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: () => uuid(),
+        type: Sequelize.UUID,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      department: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      rank: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      permission: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("accounts");
+  },
+};
