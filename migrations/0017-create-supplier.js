fe55e76d51b0e548a044bf2627545c4934141e77
Chianti Scarlett <chianti.scarlett@gmail.com>
2020-05-13 11:23:38 +0900

    Supplier 모델 추가

---
+++ migrations/0017-create-supplier.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const uuid = require("uuid");
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("suppliers", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: () => uuid(),
+        type: Sequelize.UUID,
+      },
+      address: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      crn: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      poc: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      fax: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      alias: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      worker: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      worker_poc: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      memo: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("accounts");
+  },
+};
