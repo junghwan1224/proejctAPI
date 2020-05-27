f20b07c4843b9b8ae632ccd7281946e8a7c2f88f
spark <junghwan1224@gmail.com>
2020-05-27 11:49:19 +0900

    주소 업데이트 컨트롤러 추가

--- controllers/address.js
+++ controllers/address.js
@@ -116,4 +116,71 @@ exports.readByAdmin = async (req, res) => {
   }
 };
 
-exports.createByAdmin = async (req, res) => {};
+exports.createByAdmin = async (req, res) => {
+  try {
+    const { account_id, addr_postcode, addr_primary, addr_detail } = req.body;
+
+    const transaction = await models.sequelize.transaction();
+
+    const address = await Address.findOne({
+      where: { account_id },
+      transaction,
+    });
+
+    if (address) {
+      const { postcode, primary, detail } = address.dataValues;
+
+      const obj = {
+        postcode: addr_postcode,
+        primary: addr_primary,
+        detail: addr_detail,
+      };
+
+      const addr = {
+        postcode,
+        primary,
+        detail,
+      };
+
+      if (!Object.is(JSON.stringify(obj), JSON.stringify(addr))) {
+        await Address.update(
+          {
+            postcode: addr_postcode,
+            primary: addr_primary,
+            detail: addr_detail,
+          },
+          {
+            where: { id: address.dataValues.id },
+            transaction,
+          }
+        );
+
+        await transaction.commit();
+
+        return res.status(200).send({ message: "update success" });
+      }
+    } else {
+      await Address.create(
+        {
+          account_id,
+          postcode: addr_postcode,
+          primary: addr_primary,
+          detail: addr_detail,
+        },
+        {
+          transaction,
+        }
+      );
+
+      await transaction.commit();
+
+      return res.status(200).send({ message: "create success" });
+    }
+
+    res.send({ message: "success" });
+  } catch (err) {
+    res
+      .status(400)
+      .send({ message: "에러가 발생했습니다. 다시 시도해주세요." });
+  }
+};
