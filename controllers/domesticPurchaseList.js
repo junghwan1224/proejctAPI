f0d483e26193625c8b5b220de4d5360b096aea29
spark <junghwan1224@gmail.com>
2020-05-28 11:52:48 +0900

    국내 매입 리스트 로직 수정

--- controllers/domesticPurchaseList.js
+++ controllers/domesticPurchaseList.js
@@ -1,44 +1,25 @@
 "use strict";
 
-const { Op } = require("sequelize");
-
 const DomesticPurchase = require("../models").domestic_purchase;
 const Product = require("../models").product;
 const Staff = require("../models").staff;
 const Supplier = require("../models").supplier;
 
 /**
- * req.query.singleProduct :: Boolean
- * req.query.recentLimit :: Integer
  * req.query.domestic_purchase_id
  */
 exports.readByAdmin = async (req, res) => {
-  let product_id;
-  let createdAt;
-
-  if (!req.query.domestic_purchase_id) {
-    return res.status(400).send({ message: "필요한 정보가 누락되었습니다." });
-  }
-
   let where = {};
 
-  if (req.query.singleProduct) {
+  if (req.query.domestic_purchase_id) {
     try {
       const response = await DomesticPurchase.findOne({
         where: {
           id: req.query.domestic_purchase_id,
-        },
-        include: [
-          {
-            model: Product,
-            required: true,
-            attributes: ["id"],
-          },
-        ],
+        }
       });
-      const { product, createdAt } = response.dataValues;
-      where.product_id = product.id;
-      if (req.query.recentLimit) where.createdAt = { [Op.lte]: createdAt };
+      const { product_id } = response.dataValues;
+      where.product_id = product_id;
     } catch (err) {
       console.log(err);
       return res.status(400).send();
