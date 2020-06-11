2a3481e5f090f79efc77b2fce2f98d8bf0520617
spark <junghwan1224@gmail.com>
2020-06-11 14:56:17 +0900

    라우트에서 컨트롤러 참조파일 이름 변경

--- routes/admin.js
+++ routes/admin.js
@@ -2,31 +2,35 @@ module.exports = (app) => {
   const verifyToken = require("./verifyToken");
   const permission = require("./permission");
   const PTYPE = permission.TYPE;
-
+  
   const account = require("../controllers/account");
   const accountList = require("../controllers/accountList");
+  const accountAddress = require("../controllers/accountAddress");
   const login = require("../controllers/login");
   const accountLevel = require("../controllers/accountLevel");
   const certify = require("../controllers/certify");
 
   const product = require("../controllers/product");
   const productList = require("../controllers/productList");
+  
   const orderList = require("../controllers/orderList");
   const delivery = require("../controllers/delivery");
   const deliveryList = require("../controllers/deliveryList");
   const deliveryPerUser = require("../controllers/deliveryPerUser");
   const payment = require("../controllers/payment");
+
   const creditTransaction = require("../controllers/creditTransaction");
   const creditTransactionList = require("../controllers/creditTransactionList");
-  const receiptExternal = require("../controllers/receiptExternal");
-  const roster = require("../controllers/roster");
+
   const staff = require("../controllers/staff");
   const staffList = require("../controllers/staffList");
+
   const supplier = require("../controllers/supplier");
   const supplierList = require("../controllers/supplierList");
+
   const domesticPurchase = require("../controllers/domesticPurchase");
   const domesticPurchaseList = require("../controllers/domesticPurchaseList");
-  const address = require("../controllers/address");
+  
   const inquiry = require("../controllers/inquiry");
   const inquiryList = require("../controllers/inquiryList");
 
@@ -66,9 +70,9 @@ module.exports = (app) => {
   app
     .route(ADMIN_ROUTE + "/address")
     .all(verifyToken.authAdmin)
-    .get(permission.verify(address.readByAdmin, PTYPE.READ_ACCOUNT))
-    .post(permission.verify(address.createByAdmin, PTYPE.CREATE_ACCOUNT))
-    .put(permission.verify(address.updateByAdmin, PTYPE.EDIT_ACCOUNT));
+    .get(permission.verify(accountAddress.readByAdmin, PTYPE.READ_ACCOUNT))
+    .post(permission.verify(accountAddress.createByAdmin, PTYPE.CREATE_ACCOUNT))
+    .put(permission.verify(accountAddress.updateByAdmin, PTYPE.EDIT_ACCOUNT));
   /* ----------------------------------------------------------------------- */
 
   /**
@@ -161,15 +165,6 @@ module.exports = (app) => {
     .get(
       permission.verify(creditTransactionList.readByAdmin, PTYPE.READ_ORDER)
     );
-  app
-    .route(ADMIN_ROUTE + "/receipt-external")
-    .all(verifyToken.authAdmin)
-    .get(permission.verify(receiptExternal.readByAdmin, PTYPE.READ_ORDER))
-    .post(permission.verify(receiptExternal.createByAdmin, PTYPE.CREATE_ORDER));
-  app
-    .route(ADMIN_ROUTE + "/roster")
-    .all(verifyToken.authAdmin)
-    .post(permission.verify(roster.createByAdmin, PTYPE.CREATE_ORDER));
   /* ----------------------------------------------------------------------- */
 
   /**
