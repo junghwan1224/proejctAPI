98562748cb5cb31fa33093c8b52336edb9bf4bd1
spark <junghwan1224@gmail.com>
2020-07-07 14:15:20 +0900

    controller 구조 변경

---
+++ controllers/warehouse/fields.js
@@ -0,0 +1,21 @@
+module.exports = [
+    {
+        name: "name",
+        type: "str",
+        allowNull: false,
+    },
+    
+    {
+        name: "location",
+        type: "str",
+        allowNull: false,
+    },
+
+   
+    {
+        name: "memo",
+        type: "str",
+        allowNull: false,
+        defaultValue: "",
+    },
+];
