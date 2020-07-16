17e3f5bf090f9f1bfc25f86b81b9d6824d159746
spark <junghwan1224@gmail.com>
2020-07-16 11:46:53 +0900

    Product CRUD 및 이미지 업로드 api 추가

--- controllers/product/crud.js
+++ controllers/product/crud.js
@@ -1,6 +1,17 @@
+import models from "../../models";
+import Fields from './fields';
+import validate from '../common/validate';
 
 const createByAdmin = async (req, res) => {
-    try {}
+    try {
+        if(! validate(Fields, req.body)) {
+            return res.status(400).send({ message: "필수로 입력해야 하는 정보를 입력하지 않으셨습니다." });
+        }
+
+        const product = await models.product.create(req.body);
+
+        return res.status(201).send(product);
+    }
     catch(err) {
         console.log(err);
         return res.status(400).send();
@@ -8,7 +19,15 @@ const createByAdmin = async (req, res) => {
 };
 
 const readByAdmin = async (req, res) => {
-    try {}
+    try {
+        const { product_id } = req.query;
+
+        const product = await models.product.findOne({
+            where: { id: product_id }
+        });
+
+        return res.status(200).send(product);
+    }
     catch(err) {
         console.log(err);
         return res.status(400).send();
@@ -16,7 +35,48 @@ const readByAdmin = async (req, res) => {
 };
 
 const updateByAdmin = async (req, res) => {
-    try {}
+    try {
+        if(! validate(Fields, req.body)) {
+            return res.status(400).send({ message: "필수로 입력해야 하는 정보를 입력하지 않으셨습니다." });
+        }
+
+        const response = await models.staff.findOne({
+            where: {
+            id: req.body.staff_id,
+            },
+        });
+        if (!response) {
+            return res
+                .status(400)
+                .send({ message: "해당 ID에 대한 유저 데이터가 존재하지 않습니다." });
+        }
+
+        const POSSIBLE_ATTRIBUTES = [
+            "name",
+            "unit",
+            "specification",
+            "type",
+            "price_a",
+            "price_b",
+            "price_c",
+            "price_d",
+            "price_e",
+            "essential_stock",
+            "memo",
+            "image"
+        ];
+    
+        let newData = {};
+        POSSIBLE_ATTRIBUTES.map(
+            (attribute) => (newData[attribute] = req.body[attribute])
+        );
+
+        await models.product.udpate(newData, {
+            where: { id: req.body.product_id }
+        });
+
+        return res.status(200).send();
+    }
     catch(err) {
         console.log(err);
         return res.status(400).send();
@@ -24,7 +84,15 @@ const updateByAdmin = async (req, res) => {
 };
 
 const deleteByAdmin = async (req, res) => {
-    try {}
+    try {
+        const { product_id } = req.query;
+
+        await models.product.destroy({
+            where: { id: product_id }
+        });
+
+        return res.status(200).send();
+    }
     catch(err) {
         console.log(err);
         return res.status(400).send();
