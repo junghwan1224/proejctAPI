16b156b42920a4367de5472bcd276b17eef0578d
Chianti Scarlett <chianti.scarlett@gmail.com>
2020-07-29 17:21:08 +0900

    then()을 이용한 promise  handling으로 변경

--- controllers/common/wrapper/index.js
+++ controllers/common/wrapper/index.js
@@ -5,183 +5,192 @@ class ControlWrapper {
    * Response Handler
    * @param {object} model Sequelize 모델 오브젝트
    * @param {object} fields 모델의 규격이 정의된 Field 오브젝트
-   */
-
-  constructor(model, fields) {
-    this.model = model;
-    this.fields = fields;
-    this.req = null;
-    this.res = null;
-    this.value = undefined;
-  }
-
-  /**
-   * Callback을 위한 함수
-   * @param {function} _function
-   */
-  async through(_function) {
-    if (this.value === undefined) _function();
-    return this;
-  }
-
-  /**
-   * 해당 클래스 연산 시작
    * @param {object} req server request object
    * @param {object} res server response object
    * @param {object} locator 데이터를 특정지을 수 있는 조건
    */
-  init(req, res, locator) {
+
+  async init(model, fields, req, res, locator) {
+    this.model = model;
+    this.fields = fields;
     this.req = req;
     this.res = res;
     this.locator = locator;
-    this.value = undefined;
-    return this;
+    this.status_code = undefined;
+    this.data = undefined;
+  }
+
+  setResponse({ code, data }) {
+    this.status_code = code;
+    this.data = data;
   }
 
   /**
    * 해당 클래스 연산 종료 및 response 반환
    */
-  end() {
-    return this.value;
+  send() {
+    this.res.status(this.status_code).send(this.data);
   }
 
   /**
    * 누락된 value들을 기존 데이터로 보완
    */
-  supplement() {
-    return this.through(async () => {
-      /** 기존 데이터 가져오기: */
-      const prevData = await this.model.findOne({
-        where: this.locator,
-      });
-      if (!prevData) {
-        this.value = this.res
-          .status(400)
-          .send({ message: "데이터가 존재하지 않습니다." });
-        return;
-      }
+  async supplement() {
+    if (this.status_code !== undefined) return;
 
-      let supplementedReq = prevData.dataValues;
-      supplementedReq.createdAt = undefined;
-      supplementedReq.updatedAt = undefined;
-
-      for (const key of Object.keys(this.req.body)) {
-        supplementedReq[key] = this.req.body[key];
-      }
-
-      this.req.body = supplementedReq;
+    /** 기존 데이터 가져오기: */
+    const prevData = await this.model.findOne({
+      where: this.locator,
     });
+    if (!prevData) {
+      this.setResponse({
+        code: 400,
+        data: {
+          message: "데이터가 존재하지 않습니다.",
+        },
+      });
+      return;
+    }
+
+    let supplementedReq = prevData.dataValues;
+    for (const key of Object.keys(this.req.body)) {
+      supplementedReq[key] = this.req.body[key];
+    }
+
+    supplementedReq.createdAt = undefined;
+    supplementedReq.updatedAt = undefined;
+    this.req.body = supplementedReq;
   }
 
   /**
    * 입력받은 데이터가 기준을 충족하지 않을 경우 - 400 반환
    */
   validate() {
-    return this.through(async () => {
-      if (!_validateFields(this.fields, this.req.body))
-        this.value = this.res
-          .status(400)
-          .send({ message: "필요한 패러미터가 누락되었습니다." });
-    });
+    if (this.status_code !== undefined) return;
+
+    if (!_validateFields(this.fields, this.req.body))
+      this.setResponse({
+        code: 400,
+        data: { message: "필요한 패러미터가 누락되었습니다." },
+      });
   }
 
   /**
    * 해당조건의 데이터가 존재하지 않을 경우 - 400 반환
    */
-  exist() {
-    return this.through(async () => {
+  async exist() {
+    if (this.status_code !== undefined) return;
+
+    try {
       const response = await this.model.findOne({
         where: this.locator,
       });
       if (!response) {
-        this.value = this.res
-          .status(400)
-          .send({ message: "데이터가 존재하지 않습니다." });
+        this.setResponse({
+          code: 400,
+          data: { message: "데이터가 존재하지 않습니다." },
+        });
       }
-    });
+    } catch (err) {
+      console.log(err);
+      this.setResponse({
+        code: 400,
+        data: { message: "에러가 발생했습니다." },
+      });
+    }
   }
 
   /**
    * 해당조건의 데이터가 존재할 경우 - 400 반환
    */
-  unique() {
-    return this.through(async () => {
-      const response = await this.model.findOne({
+  async unique() {
+    if (this.status_code !== undefined) return;
+
+    const response = await this.model.findOne({
+      where: this.locator,
+    });
+    if (response) {
+      this.setResponse({
+        code: 400,
+        data: { message: "데이터가 이미 존재합니다." },
+      });
+    }
+  }
+
+  async post() {
+    if (this.status_code !== undefined) return;
+
+    try {
+      const response = await this.model.create(this.req.body);
+      this.setResponse({
+        code: 201,
+        data: { message: response.id },
+      });
+    } catch (err) {
+      console.log(err);
+      this.setResponse({
+        code: 400,
+        data: { message: "에러가 발생했습니다." },
+      });
+    }
+  }
+
+  async get() {
+    if (this.status_code !== undefined) return;
+
+    try {
+      const response = await this.model.findOne({ where: this.locator });
+      this.setResponse({
+        code: 200,
+        data: response,
+      });
+    } catch (err) {
+      console.log(err);
+      this.setResponse({
+        code: 400,
+        data: { message: "에러가 발생했습니다." },
+      });
+    }
+  }
+
+  async patch() {
+    if (this.status_code !== undefined) return;
+
+    try {
+      await this.model.update(this.req.body, {
         where: this.locator,
       });
-      if (response) {
-        this.value = this.res
-          .status(400)
-          .send({ message: "데이터가 이미 존재합니다." });
-      }
-    });
+      this.setResponse({
+        code: 200,
+        data: { message: "성공적으로 업데이트 되었습니다." },
+      });
+    } catch (err) {
+      console.log(err);
+      this.setResponse({
+        code: 400,
+        data: { message: "에러가 발생했습니다." },
+      });
+    }
   }
 
-  post() {
-    return this.through(async () => {
-      try {
-        const response = await this.model.create(this.req.body);
-        this.value = this.res.status(201).send({ message: response.id });
-      } catch (err) {
-        console.log(err);
-        this.value = this.res
-          .status(400)
-          .send({ message: "에러가 발생했습니다." });
-      }
-    });
-  }
+  async delete() {
+    if (this.status_code !== undefined) return;
 
-  get() {
-    return this.through(async () => {
-      try {
-        const response = await this.model.findOne({ where: this.locator });
-        if (!response) {
-          this.value = this.res
-            .status(400)
-            .send({ message: "데이터가 존재하지 않습니다." });
-        }
-        this.value = this.res.status(200).send(response);
-      } catch (err) {
-        console.log(err);
-        this.value = this.res
-          .status(400)
-          .send({ message: "에러가 발생했습니다." });
-      }
-    });
-  }
-
-  patch() {
-    return this.through(async () => {
-      try {
-        await this.model.update(this.req.body, {
-          where: this.locator,
-        });
-        this.value = this.res
-          .status(200)
-          .send({ message: "성공적으로 업데이트 되었습니다." });
-      } catch (err) {
-        console.log(err);
-        this.value = this.res
-          .status(400)
-          .send({ message: "에러가 발생했습니다." });
-      }
-    });
-  }
-
-  delete() {
-    return this.through(async () => {
-      try {
-        await this.model.destroy({
-          where: this.locator,
-        });
-        this.value = this.res.status(200).send();
-      } catch (err) {
-        console.log(err);
-        this.value = this.res
-          .status(400)
-          .send({ message: "에러가 발생했습니다." });
-      }
-    });
+    try {
+      await this.model.destroy({
+        where: this.locator,
+      });
+      this.setResponse({
+        code: 200,
+        data: undefined,
+      });
+    } catch (err) {
+      console.log(err);
+      this.setResponse({
+        code: 400,
+        data: { message: "에러가 발생했습니다." },
+      });
+    }
   }
 }
 
--- controllers/product/crud.js
+++ controllers/product/crud.js
@@ -1,36 +1,59 @@
 import models from "../../models";
 import Fields from "./fields";
-import ControlWrapper from "../common/wrapper";
-
-const wrapper = new ControlWrapper(models.product, Fields);
+import Controller from "../common/wrapper";
 
 const createByAdmin = async (req, res) => {
   const locator = {
     name: req.body.name,
   };
-  return wrapper.init(req, res, locator).unique().validate().post().end();
+  const controller = new Controller();
+
+  controller
+    .init(models.product, Fields, req, res, locator)
+    .then(() => controller.unique())
+    .then(() => controller.validate())
+    .then(() => controller.post())
+    .then(() => controller.send());
 };
 
 const readByAdmin = async (req, res) => {
   const locator = {
     id: req.query.product_id,
   };
+  const controller = new Controller();
 
-  return wrapper.init(req, res, locator).get().end();
+  controller
+    .init(models.product, Fields, req, res, locator)
+    .then(() => controller.exist())
+    .then(() => controller.get())
+    .then(() => controller.send());
 };
 
 const updateByAdmin = async (req, res) => {
   const locator = {
     id: req.body.product_id,
   };
-  return wrapper.init(req, res, locator).exist().supplement().patch().end();
+  const controller = new Controller();
+
+  controller
+    .init(models.product, Fields, req, res, locator)
+    .then(() => controller.exist())
+    .then(() => controller.supplement())
+    .then(() => controller.patch())
+    .then(() => controller.send());
 };
 
 const deleteByAdmin = async (req, res) => {
   const locator = {
     id: req.query.product_id,
   };
-  return wrapper.init(req, res, locator).exist().delete().end();
+  const controller = new Controller();
+
+  controller
+    .init(models.product, Fields, req, res, locator)
+    .then(() => controller.exist())
+    .then(() => controller.delete())
+    .then(() => controller.send());
 };
 
 export default { readByAdmin, createByAdmin, updateByAdmin, deleteByAdmin };
